4017505827001d05fcc1588c39f59bb4d6324b8c
khushpreetsingh <khushsran189@gmail.com>
2018-01-25 22:18:50 -0500

    LAB 2

---
+++ app/src/main/java/khushpreetsingh/androidlabs/ListItemsActivity.java
@@ -0,0 +1,13 @@
+package khushpreetsingh.androidlabs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListItemsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_items);
+    }
+}
